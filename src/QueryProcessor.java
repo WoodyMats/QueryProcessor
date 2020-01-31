f0c1dcb1f483a9f8b48de1651e9b736243518fb3
Dimitrios Moutafidis <dmoutafi@csd.auth.gr>
2020-01-31 15:03:21 +0200

    V9

--- src/QueryProcessor.java
+++ src/QueryProcessor.java
@@ -29,7 +29,8 @@ public class QueryProcessor {
      * @param search Είσοδος χρήστη.
      */
     QueryProcessor(String search)  {
-       termToSearch =search.split(" ");
+
+        termToSearch =search.toLowerCase().split(" ");
     }
 
 
